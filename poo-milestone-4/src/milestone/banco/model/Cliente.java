62b3bd4d0e19d3f5907ca1f7f459226aa2bd773c
GuilhermeMth <guilhermemfranca06@gmail.com>
2025-05-22 19:27:36 -0300

    milestone 4 adicionada

---
+++ poo-milestone-4/src/milestone/banco/model/Cliente.java
@@ -0,0 +1,91 @@
+package milestone.banco.model;
+
+import java.util.ArrayList;
+
+/*
+ * Programação Orientada a Objetos
+ * Milestone 2
+ * Discente: Guilherme Matheus França da Silva
+ * 
+ */
+public class Cliente {
+
+	private String cpf;
+    private String nome;
+    private ArrayList<Conta> contas;
+
+    //Não alterar o construtor
+    public Cliente(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
+    
+    /*
+     * Método recebe uma conta a ser adicionada no arraylist de contas
+     * do cliente. Antes de adicioná-la, deve-se verificar se já existe uma
+     * conta existente. Para uma adição realizada com sucesso, deve-se retornar 
+     * true. Caso contrário, false.
+     */
+    public boolean adicionarConta(Conta conta) {
+        if (contas.contains(conta)) {
+			return false;
+		}else
+			contas.add(conta);
+        	return true;
+    }
+
+    /*
+     * Método recebe um número de conta a ser removida do arraylist de contas
+     * do cliente. Antes de remove-la, deve-se verificar se a conta existe no
+     * array. Para tanto, implemente o método localizarConta deste método, pois 
+     * convém utilizá-lo no escopo deste método. Para remoção realizada com sucesso, 
+     * deve-se retornar true. Caso contrário, false.
+     */
+	public boolean removerConta(String numeroConta) {
+		
+		Conta conta = localizarConta(numeroConta);
+		if (conta != null) {
+			contas.remove(conta);
+			return true;
+		}else
+			return false;
+			
+	}
+
+    /*
+     * Método recebe uma número de conta como argumento a fim
+     * de verificar se existe o elemento com tal número no array.
+     * Caso exista, retorne o elemento consultado, caso contrário
+     * retorne null.
+     * 
+     */
+    public Conta localizarConta(String numeroConta) {
+    	Conta conta = new Conta(numeroConta);
+    	int index = contas.indexOf(conta);
+    	
+    	if (index == -1) {
+			return null;
+		}else
+			return contas.get(index);
+    }
+    
+
+    //Não alterar os métodos abaixo
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+ 
+}
